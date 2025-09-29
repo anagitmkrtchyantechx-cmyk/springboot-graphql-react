708947916b64557b50dd73442bf793fd2d71b759
CM-ARM-06-L <anagit,mkrtchyan.techx@gmail.com>
2025-09-29 19:17:13 +0400

    Backend part implemented

---
+++ src/main/java/com/anita/Author.java
@@ -0,0 +1,21 @@
+package com.anita;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public record Author(Integer id,
+                     String name) {
+    public static List<Author> authors = Arrays.asList(
+            new Author(1, "Anita"),
+            new Author(2, "Monica"),
+            new Author(3, "Alisa")
+
+    );
+
+    public static Optional<Author> getAuthorById(Integer id) {
+        return authors.stream()
+                .filter(b -> b.id.equals(id))
+                .findFirst();
+    }
+}
